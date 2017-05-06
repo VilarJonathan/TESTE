66d11c712bb7e587087dfac07b73d8e6078cdfb3
Jonathan Vilar <Jonathan Vilar@DESKTOP-CQH0FJM>
2017-05-06 15:35:43 -0300

    Criei a classe

---
+++ src/Conta.java
@@ -0,0 +1,25 @@
+
+public class Conta {
+
+    int numero;
+    String dono;
+    double saldo;
+    double limite;
+    int saca;
+    int deposita;
+
+    void deposita(double quantidade) {
+        this.saldo += quantidade;
+    }
+
+    boolean saca(double valor) {
+
+        if (this.saldo < valor) {
+            return false;
+        } else {
+            this.saldo = this.saldo - valor;
+            return true;
+        }
+    }
+
+}
